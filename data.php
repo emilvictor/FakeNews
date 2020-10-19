c1bc0269ca671c4276dea040d00557ff2046682d
Emil Pettersson <61893425+emilvictor@users.noreply.github.com>
2020-10-19 11:23:50 +0200

    added authors array to data.php

--- data.php
+++ data.php
@@ -2,5 +2,16 @@
 
 declare(strict_types=1);
 
+$authors = [
+
+    'ID' => 'Astrid Lundgren',
+    'ID' => 'Selma Lagerhof',
+    'ID' => 'Jan Guldski',
+    'ID' => 'August Stravinski',
+    'ID' => 'Villiam Morbeck',
+];
+
+
+
 // This is the file where you can keep your data arrays such as articles and
 // authors.
